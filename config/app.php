0b49e076aa8a81308e116a571e3d166411a24f74
Tomas Gonzalez Zapata <tomasgz2006@gmail.com>
2025-02-02 10:09:08 -0500

    Configuracion del Autoload, y de la App

---
+++ config/app.php
@@ -0,0 +1,6 @@
+<?php
+	const APP_URL="http://localhost/pruebatecnica/";
+	const APP_NAME="Acema Ingenieria Prueba";
+	const APP_SESSION_NAME="pruebatecnica";
+
+	date_default_timezone_set("America/Bogota");
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    require_once "./config/app.php";
+    require_once "./autoload.php";
+?>
